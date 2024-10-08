94e396a6ca7c5b626326f424923e8145ffbd1e1f
dafandikri <dafandikri@gmail.com>
2024-10-08 11:44:58 +0700

    Added GitHub Actions for Push to PWS

--- halal_bites/settings.py
+++ halal_bites/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://erdafa-andikri-halalbites.pbp.cs.ui.ac.id", "https://erdafa-andikri-halalbites.pbp.cs.ui.ac.id"]
