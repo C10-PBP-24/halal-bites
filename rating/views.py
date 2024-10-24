89d44852d4dd3cc7d7b988a5da25afa1214fe3f9
dafandikri <dafandikri@gmail.com>
2024-10-24 21:51:04 +0700

    Renamed review into rating_form

--- rating/views.py
+++ rating/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import RatingForm
 
-def show_review(request):
-    return render(request, "review.html")
+def show_rating_form(request):
+    return render(request, "rating_form.html")
 
 def create_rating(request):
     if request.method == 'POST':
         form = RatingForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('rating:show_review')  # Replace 'some_view_name' with the name of the view you want to redirect to
+            return redirect('rating:show_rating_form')  # Replace 'some_view_name' with the name of the view you want to redirect to
     else:
         form = RatingForm()
-    return render(request, 'review.html', {'form': form})  # Replace 'template_name.html' with your template name
+    return render(request, 'rating_form.html', {'form': form})  # Replace 'template_name.html' with your template name
