50ddc8f4935e38b8f406f8bcf6b0ee2cd5c4696a
Imam Fajri <imamfajri.ze@gmail.com>
2024-10-24 11:42:32 +0700

    Added Food Json and configurate model in dictory food

--- food/models.py
+++ food/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 class Food(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='food_images/', null=True, blank=True)
-
-    def __str__(self):
-        return self.name
+    promo = models.CharField(max_length=255)
