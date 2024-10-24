147b8921abed18d306986ca3e05caca9682884c5
emanuellaabygail <abygailsiahaan1911@gmail.com>
2024-10-24 19:54:12 +0700

    database for resto

--- halal_bites/settings.py
+++ halal_bites/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'food',
+    'resto',
+    'forum',
+    'rating',
 ]
 
 MIDDLEWARE = [
--- resto/views.py
+++ resto/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+
+from models import Resto
 
 # Create your views here.
+def get_resto(request):
+    data = Resto.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
