4a906ee88c4115c352dfe2a88e6279317641090d
Imam Fajri <imamfajri.ze@gmail.com>
2024-10-25 10:50:53 +0700

    added fitur food detail and show food

--- food/views.py
+++ food/views.py
@@ -1,17 +1,35 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.core import serializers
 from .models import Food
 from django.contrib.auth.decorators import login_required
+from authentication.models import UserProfile
 
 
-@login_required
+@login_required(login_url="authentication:login")
 def show_food(request):
+    user = request.user
+    user_profile = UserProfile.objects.get(user=user)
 
-    return(request)
+    foods = Food.objects.all()
 
-def get_foods(request):
+    context = {
+        'products' : foods
+    }
+
+    return render(request, 'menu.html', context)
+
+@login_required(login_url="authentication:login")
+def food_detail(requets, food_id):
+    food = get_object_or_404(Food, food_id)
+    context = {'food': food}
+    return render(requets, 'food_detail.html', context)
+
+def get_food(request):
     data = Food.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json" )
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 
+def get_food_by_id(request, id):
+    data = Food.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
