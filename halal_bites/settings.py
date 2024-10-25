0812c02feee2d60dccc235c3cc3e0c1ef6470136
Syahirahputrii <syahirahputri04@gmail.com>
2024-10-25 12:06:45 +0700

    tracker

--- halal_bites/settings.py
+++ halal_bites/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'food',
+    'resto',
+    'rating',
+    'forum',
     'tracker'
 ]
 
--- resto/models.py
+++ resto/models.py
@@ -1,2 +1,7 @@
 from django.db import models
+from food.models import Food
 
+class Resto(models.Model):
+    nama = models.CharField(max_length=30)
+    makanan = models.CharField(max_length=100)
+    lokasi = models.CharField(max_length=30)
