f78b387adaa155b014552e43642d09ce3a636206
emanuellaabygail <abygailsiahaan1911@gmail.com>
2024-10-25 09:29:15 +0700

    resto page

--- resto/views.py
+++ resto/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 
-from models import Resto
+from resto.models import Resto
 
 # Create your views here.
 def get_resto(request):
     data = Resto.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_resto(request):
+    data = Resto.objects.all()
+    return render(request, 'resto/main.html', {'restos': data})
