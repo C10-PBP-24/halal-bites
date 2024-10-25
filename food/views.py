2be5793d9e8d51013ca28d96bac46f54443b0a1f
Imam Fajri <imamfajri.ze@gmail.com>
2024-10-25 13:32:40 +0700

    added fitur filter, add food, get food, get food by id, and added directory templates in project

--- food/views.py
+++ food/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.core import serializers
-from .models import Food
+from food.models import Food
 from django.contrib.auth.decorators import login_required
 from authentication.models import UserProfile
+from django.views.decorators.csrf import csrf_exempt
 
 
 @login_required(login_url="authentication:login")
-def show_food(request):
+def show_menu(request):
     user = request.user
     user_profile = UserProfile.objects.get(user=user)
 
@@ -25,6 +26,19 @@ def food_detail(requets, food_id):
     context = {'food': food}
     return render(requets, 'food_detail.html', context)
 
+@csrf_exempt
+def add_food(request):
+    if request.method == "POST":
+        name = request.POST.get('Name')
+        price = request.POST.get('Price')
+        image = request.POST.get('Image')
+        promo = request.POST.get('Promo')
+
+        new_food = Food(name=name, price=price, image=image, promo=promo)
+        new_food.save()
+        return HttpResponse(b"CREATED", status=201)
+    return HttpResponseNotFound()
+
 def get_food(request):
     data = Food.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
@@ -33,3 +47,15 @@ def get_food(request):
 def get_food_by_id(request, id):
     data = Food.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@csrf_exempt
+def filter_food(request):
+    price = request.GET.get('price', '')
+
+    foods = Food.objects.all()
+    if price:
+        foods = foods.filter(price__icontains=price)
+
+    foods_json = serializers.serialize('json', foods)
+    return JsonResponse(foods_json, safe=False)
+
