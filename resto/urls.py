9710516587b01b5f6399398e6fbaf286b8a0be91
emanuellaabygail <abygailsiahaan1911@gmail.com>
2024-10-25 23:08:24 +0700

    added filter by name, added detail page

--- resto/urls.py
+++ resto/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from resto.views import get_resto, show_resto, filter_resto, add_resto
-from resto.views import delete_resto
+from resto.views import delete_resto, resto_detail
 
 app_name = "resto"
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', show_resto, name="show_resto"),
     path('filter-resto/', filter_resto, name='filter_resto'),
     path('add-resto/', add_resto, name='add_resto'),
-    path('delete-resto/<int:id>/', delete_resto, name="delete_resto")
+    path('delete-resto/<int:id>/', delete_resto, name="delete_resto"),
+    path('detail/<int:pk>/', resto_detail, name="resto_detail")
 ]
