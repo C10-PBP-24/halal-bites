6a7978eb3286859d246a586eba9ce5bca201912b
dafandikri <dafandikri@gmail.com>
2024-10-27 16:46:01 +0700

    Refactor Tracker model and view for better user experience

--- tracker/views.py
+++ tracker/views.py
@@ -20,12 +20,17 @@ def add_food_tracking(request):
         if form.is_valid():
             food_tracking = form.save(commit=False)
             food_tracking.user = request.user
+            food_tracking.restaurant = food_tracking.food.resto  # Assuming each food has a related restaurant
             food_tracking.save()
             return redirect('tracker:food_tracker')
     else:
-        ordered_foods = Food.objects.filter(ratings__user=request.user)
+        ordered_foods = Food.objects.filter(ratings__user=request.user).distinct()
         user_ratings = Rating.objects.filter(user=request.user)
         form = AddFoodTrackingForm()
+
+    # Print statements for debugging
+    print("Ordered Foods:", ordered_foods)
+    print("User Ratings:", user_ratings)
 
     return render(request, 'tracker.html', {
         'form': form,
