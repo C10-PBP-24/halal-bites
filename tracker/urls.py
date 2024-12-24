3902b9c3135824b343ae7eb090febdca049cb520
emanuellaabygail <abygailsiahaan1911@gmail.com>
2024-12-24 12:38:02 +0700

    fix food tracker url

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'tracker'
 
 urlpatterns = [
+    path('food_tracker', views.food_tracker, name='food_tracker'),
     path('json/', views.show_json, name='show_json'),
     path('add/', views.add_tracker, name='add_tracker'),
 ]
